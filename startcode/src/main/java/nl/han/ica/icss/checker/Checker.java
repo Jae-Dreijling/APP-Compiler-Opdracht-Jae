f4515232892fe0ddcf1b187caa8df38411b80072
Osomizu <j.dreijling@student.han.nl>
2026-04-03 15:54:25 +0200

    set up the minimal stuff

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -3,53 +3,112 @@ package nl.han.ica.icss.checker;
 import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
-import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
 
 public class Checker {
 
-    private IHANLinkedList<HashMap<String, ExpressionType>> variableTypes;
+    // Stack of scopes (each scope = variable name → type)
+    private IHANLinkedList<HashMap<String, Object>> variableScopes;
 
     public void check(AST ast) {
-        variableTypes = new HANLinkedList<>();
-        variableTypes.addFirst(new HashMap<>());
+        variableScopes = new HANLinkedList<>();
 
+        // Global scope
+        pushScope();
+
+        // Walk root
         for (ASTNode node : ast.root.getChildren()) {
-            if (node instanceof VariableAssignment) {
-                checkVariableAssignment((VariableAssignment) node);
-            } else if (node instanceof Stylerule) {
-                checkStylerule((Stylerule) node);
-            }
+            walk(node);
         }
 
-        variableTypes.removeFirst();
+        popScope();
     }
 
-    private void checkStylerule(Stylerule stylerule) {
-        for (ASTNode node : stylerule.getChildren()) {
-            if (node instanceof Declaration) {
-                checkDeclaration((Declaration) node);
-            }
+    // -------------------------
+    // TREE WALKER
+    // -------------------------
+
+    private void walk(ASTNode node) {
+
+        if (node instanceof Stylerule) {
+            enterStylerule((Stylerule) node);
+        }
+        else if (node instanceof IfClause) {
+            enterIfClause((IfClause) node);
+        }
+        else if (node instanceof ElseClause) {
+            enterElseClause((ElseClause) node);
+        }
+        else if (node instanceof VariableAssignment) {
+            enterVariableAssignment((VariableAssignment) node);
+        }
+        else if (node instanceof Declaration) {
+            enterDeclaration((Declaration) node);
+        }
+
+        // Always continue walking children
+        for (ASTNode child : node.getChildren()) {
+            walk(child);
+        }
+
+        // Exit hooks
+        if (node instanceof Stylerule) {
+            exitStylerule();
+        }
+        else if (node instanceof IfClause) {
+            exitIfClause();
+        }
+        else if (node instanceof ElseClause) {
+            exitElseClause();
         }
     }
 
-    private void checkVariableAssignment(VariableAssignment assignment) {
-        // for now: just store variable
-        variableTypes.getFirst().put(assignment.name.name, ExpressionType.UNDEFINED);
+    // -------------------------
+    // SCOPES
+    // -------------------------
+
+    private void pushScope() {
+        variableScopes.addFirst(new HashMap<>());
     }
 
-    private void checkDeclaration(Declaration declaration) {
-        checkExpression(declaration.expression);
+    private void popScope() {
+        variableScopes.removeFirst();
     }
 
-    private void checkExpression(Expression expr) {
-        if (expr instanceof VariableReference) {
-            String name = ((VariableReference) expr).name;
+    // -------------------------
+    // ENTER METHODS (EMPTY FOR NOW)
+    // -------------------------
 
-            if (!variableTypes.getFirst().containsKey(name)) {
-                expr.setError("Undefined variable: " + name);
-            }
-        }
+    private void enterStylerule(Stylerule rule) {
+        pushScope();
+    }
+
+    private void exitStylerule() {
+        popScope();
+    }
+
+    private void enterIfClause(IfClause ifClause) {
+        pushScope();
+    }
+
+    private void exitIfClause() {
+        popScope();
+    }
+
+    private void enterElseClause(ElseClause elseClause) {
+        pushScope();
+    }
+
+    private void exitElseClause() {
+        popScope();
+    }
+
+    private void enterVariableAssignment(VariableAssignment assignment) {
+        // Step 2: implement
+    }
+
+    private void enterDeclaration(Declaration declaration) {
+        // Step 2: implement
     }
 }
